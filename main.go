e10ad72492b5557171f47e14a8219c2b71a6ccf4
Connor Hudson <connorqhudson2020@gmail.com>
2025-09-03 11:48:33 -0500

    use LookupEnv instead of Getenv for better error handling

--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
-    "log"
-    "os" // needed to load env variables loaded from godotenv
+	"log"
+	"os" // needed to load env variables loaded from godotenv
 
-    "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-    
-    // First load the .env file
-    log.Print("Loading .env file")
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
-    log.Print(".env file successfully loaded")
 
-    // Fetch Telegram API token from .env
-    telegramToken := os.Getenv("TG_API_KEY")
-    log.Printf("Your Telegram API token is %s", telegramToken) // This line is for local debugging and should be disabled when running in prod
+	// First load the .env file
+	log.Print("Loading .env file")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	log.Print(".env file successfully loaded")
+
+	// Attempt to fetch Telegram API token after .env is loaded
+	telegramToken, telegramTokenExists := os.LookupEnv("TG_API_KEY")
+	if telegramTokenExists == false {
+		log.Fatal("Error fetching TG_API_KEY environment variable")
+	}
+	log.Printf("Your Telegram API token is %s", telegramToken) // This line is for local debugging and should be disabled when running in prod
 }
