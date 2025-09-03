19fd1502427445deb0fa4e7b2813c7e02d38a9e8
Connor Hudson <connorqhudson2020@gmail.com>
2025-09-03 11:41:03 -0500

    load .env file and fetch telegram api token

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-    "fmt"
+    "log"
+    "os" // needed to load env variables loaded from godotenv
+
+    "github.com/joho/godotenv"
 )
 
 func main() {
-    fmt.Print("test")
+    
+    // First load the .env file
+    log.Print("Loading .env file")
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+    log.Print(".env file successfully loaded")
+
+    // Fetch Telegram API token from .env
+    telegramToken := os.Getenv("TG_API_KEY")
+    log.Printf("Your Telegram API token is %s", telegramToken) // This line is for local debugging and should be disabled when running in prod
 }
