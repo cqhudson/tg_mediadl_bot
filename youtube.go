26d4a7c1125578d38b78d9409067a60323387b3d
Connor Hudson <connorqhudson2020@gmail.com>
2025-09-05 23:23:26 -0500

    Begin creating func to extract youtube ID from a given URL

--- youtube.go
+++ youtube.go
@@ -3,6 +3,9 @@ package main
 import (
     "log"
     "os/exec"
+    "errors"
+
+    "github.com/dlclark/regexp2"
 )
 
 func downloadYouTubeVideo(url string) error {
@@ -22,4 +25,38 @@ func downloadYouTubeVideo(url string) error {
 	return nil
 }
 
+func extractYouTubeId(url *regexp2.Match) string, error { 
+    fullUrl := string(url)
+    domain := "" 
+    index := 0
 
+    // First let's grab the domain in the URL 
+    for i, letter := range fullUrl {
+        domain += string(letter)
+        if domain == "youtu.be" || domain == "youtube.com" {
+            index = i
+            break  
+        }
+    }
+    if domain != "youtu.be" || domain != "youtube.com" {
+        // Something really bad happened if you hit this block :(
+        return "", errors.New("(extractYouTubeId func) - no valid YouTube domain could be extracted" 
+    }
+
+
+    youtubeId := ""
+
+    if domain == "youtu.be" {
+        // index+1 = the "/" char after the domain
+        // TODO FIX THIS FOR LOOP TO USE PROPER INDEXING!!!
+        for i := index+1, letter := range fullUrl {
+            if string(letter) == "?" || string(letter) == "\n" {
+                return youtubeId, nil
+            }
+            youtubeId += string(letter)
+        }
+    }
+
+    if domain == "youtube.com" {
+    }
+}
