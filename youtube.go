7f873481c3398fa3762b27cce1e42b3db1b198c6
Connor Hudson <connorqhudson2020@gmail.com>
2025-09-19 18:43:23 -0500

    add capability to check for existing download before redownloading a file

--- youtube.go
+++ youtube.go
@@ -7,12 +7,37 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/dlclark/regexp2"
 )
 
 func downloadYouTubeVideo(url string, youtubeId string) (*os.File, error) {
+	
+	// First let's check for an existing video
+	//
+	existingDownload, err := checkVideoAlreadyDownloaded("download/yt", youtubeId)
+	if err != nil {
+		log.Printf("There was an error trying to check for existing download --> %s", err.Error())
+	}
+	
+	// If an existing download was found, let's return a pointer to it instead of redownloading
+	//
+	if existingDownload != "" {
+		log.Printf("Existing file found at --> %s", existingDownload)
 
+		file, err := os.Open(existingDownload)
+		if err != nil {
+			log.Printf("There was an error trying to fetch a pointer to the file --> %s", err.Error())
+		} else {
+			log.Printf("Got a pointer to the file --> %+v", file)
+			return file, nil
+		}	
+
+	}
+
+	// If a previously downloaded video doesn't exist, let's attempt to download
+	//
 	binary, err := exec.LookPath("exec/yt-dlp")
 	if err != nil {
 		log.Printf("Failed to LookPath --> %s", err.Error())
@@ -31,6 +56,7 @@ func downloadYouTubeVideo(url string, youtubeId string) (*os.File, error) {
 		outputOption,
 		compressionOptions,
 		"-f",
+		// **should** force a download in mp4 format
 		"bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
 		//"--recode-video",
 		//"mp4",
@@ -105,6 +131,7 @@ func extractYouTubeId(url *regexp2.Match) (string, error) {
 		return youtubeId, nil
 	}
 
+	// TODO: This is broken, need to fix support for youtube.com domain parsing
 	if domain == "https://youtube.com" || domain == "http://youtube.com" || domain == "https://www.youtube.com" || domain == "http://www.youtube.com" {
 		temp := ""
 		for i := index + 2; i < len(fullUrl); i++ {
@@ -128,4 +155,31 @@ func extractYouTubeId(url *regexp2.Match) (string, error) {
 	}
 
 	return "", errors.New("Unable to extract a YouTube ID")
+}
+
+// if the video is already downloaded, then we can send the existing video
+func checkVideoAlreadyDownloaded(dir string, filename string) (string, error) {
+    var filePath string	
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err 
+		}
+		if info.IsDir() == false {
+			// Get filename without extension since we don't necessarily know
+			// what the ext will be (mp4, webm, etc)
+			nameWithoutExt := strings.TrimSuffix(info.Name(), filepath.Ext(info.Name()))
+			if nameWithoutExt == filename {
+				filePath = path
+				return filepath.SkipDir
+			}
+		}
+		return nil 
+	})
+	if err != nil {
+		return "", err
+	}
+	if filePath == "" {
+		return "", fmt.Errorf("Failed to find existing file with base name of %s", filename)
+	} 
+	return filePath, nil
 }
