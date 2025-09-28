90eb73993524c9b02919ee368c723c7c3123503d
Connor Hudson <connorqhudson2020@gmail.com>
2025-09-28 16:16:20 -0500

    move youtube download logic into a single func and store in youtube.go

--- main.go
+++ main.go
@@ -76,139 +76,15 @@ func main() {
 
 		l.Printf("Update:  %+v", update)
 
-		// Message.Text contains the exact text the user sent the bot
-		//
-		message := update.Message.Text
-
-		// Message.From.Username returns the username of the account who sent the msg
-		//
-		username := update.Message.From.Username
-
-
 		// This regex is DISGUSTING and makes me sad
 		//
 		const ytRegex string = `https?://(?:www\.|m\.)?youtube\.com/watch\?v=[A-Za-z0-9_-]{11}(?:\?si=[A-Za-z0-9_-]+)?|https?://youtu\.be/[A-Za-z0-9_-]{11}(?:\?si=[A-Za-z0-9_-]+)?`
 		l.Printf("Checking for YouTube video using the following regex --> %s", ytRegex)
 
-		containsYouTubeLink := checkForYouTubeLinks(message, ytRegex, true)
+		containsYouTubeLink := checkForYouTubeLinks(update.Message.Text, ytRegex, true)
 
 		if containsYouTubeLink {
-
-			// If the message contains a youtube link, attempt to extract it from the message
-			//
-			l.Print("The message contained a valid YouTube link. Attempting to extract it from the message.")
-
-			url, err := extractUrl(message, ytRegex, true)
-
-			if err != nil {
-				l.Printf("There was an issue extracting the YouTube URL --> %s", err.Error())
-
-				msg := "There was an issue extracting the youtube link from your message. Please ensure your URL is valid"
-				err = sendTelegramMessage(bot, &update, msg)
-
-				if err != nil {
-					l.Printf("Failed to send message to %s --> %s", username, err.Error())
-				} else {
-					l.Printf("Successfully send %s the following Telegram message --> %s", username, msg)
-				}
-
-				continue
-			}
-
-			l.Printf("The extraction returned the following --> %+v", url)
-			//
-			////
-
-			// If the URL is valid, we need to extract the YT ID from it
-			//
-			l.Print("Attempting to extract the ID from the YouTube link.")
-
-			youtubeId, err := extractYouTubeId(url)
-
-			if err != nil {
-				l.Printf("There was an error trying to extract a YouTube ID from the given URL: %s", err.Error())
-				
-				msg := "There was an issue extracting the youtube ID from the link you sent. Please ensure your URL is valid."
-				err = sendTelegramMessage(bot, &update, msg)
-
-				if err != nil {
-					l.Printf("Failed to send message to %s --> %s", username, err.Error())
-				} else {
-					l.Printf("Successfully sent %s the following Telegram message --> %s", username, msg)
-				}
-
-				continue
-			}
-
-			l.Printf("The extracted YouTube ID was --> %s", youtubeId)
-			//
-			////
-
-			// Attempt to download the video. If it is already downloaded, we will send the downloaded file. 
-			//
-			l.Printf("Attempting to download YouTube video")
-
-			msg := "Attempting to download YouTube video. Please be patient as it can take a few minutes to finish the download."
-			err = sendTelegramMessage(bot, &update, msg)
-
-			if err != nil {
-				l.Printf("Failed to send message to %s --> %s", username, err.Error())
-			} else {
-				l.Printf("Successfully sent %s the following Telegram message --> %s", username, msg)
-			}
-
-			downloadedVideo, err := downloadYouTubeVideo(message, youtubeId)
-
-			if err != nil {
-				l.Printf("There was an error trying to download the video: %s", err.Error())
-
-				msg := "Failed to download video. I appologize for the inconvenience. Please wait a little bit and try again later."
-				err = sendTelegramMessage(bot, &update, msg)
-				 
-				if err != nil {
-					l.Printf("Failed to send message to %s --> %s", username, err.Error())
-				} else { 
-					l.Printf("Successfully sent %s the following Telegram message --> %s", username, msg)
-				}
-
-				continue
-			}
-
-			l.Printf("The downloaded video was found --> %+v", downloadedVideo)
-			//
-			////
-
-			// Let's send a message letting the user know that the video is being sent
-			//
-			l.Printf("Attempting to send a message to %s to let them know we are attempting to send them a file", username)
-
-			msg = "Successfully downloaded the video. Attempting to send it to you. Please be patient as larger videos may take some time to send"
-			err = sendTelegramMessage(bot, &update, msg)
-
-			// TODO: Add error handling here
-
-			l.Printf("Successfully sent the following message to %s --> %+v", username, msg)
-
-			//
-			////
-
-			// let's send the downloaded video to the user now
-			// func (b *Bot) SendVideo(ctx context.Context, params *SendVideoParams) (*Message, error)
-			//
-			l.Printf("Attempting to send video file to %s", username)
-			
-			err = SendTelegramVideo(bot, &update, downloadedVideo)	
-
-			if err != nil {
-				l.Printf("An error occurred while sending the video --> %s", err)
-				continue
-			}
-
-			l.Printf("Successfully sent the video to %s", username)
-			//
-			////
-
-			continue
+			handleYouTubeVideo(&update, ytRegex, bot);
 		}
 
 	}
--- youtube.go
+++ youtube.go
@@ -9,8 +9,10 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/mymmrac/telego"
 	"github.com/dlclark/regexp2"
 	"github.com/cqhudson/logger"
+	
 )
 
 func downloadYouTubeVideo(url string, youtubeId string) (*os.File, error) {
@@ -223,4 +225,126 @@ func checkVideoAlreadyDownloaded(dir string, filename string) (string, error) {
 		return "", fmt.Errorf("Failed to find existing file with base name of %s", filename)
 	}
 	return filePath, nil
+}
+
+// This is called in main.go to facilitate downloading a YouTube video
+func handleYouTubeVideo(update *telego.Update, ytRegex string, bot *telego.Bot) {
+
+	l := logger.NewLogger(true, false, false)
+
+	// Message.Text contains the exact text the user sent the bot
+	//
+	message := update.Message.Text
+
+	// Message.From.Username returns the username of the account who sent the msg
+	//
+	username := update.Message.From.Username
+
+
+	// If the message contains a youtube link, attempt to extract it from the message
+	//
+	l.Print("The message contained a valid YouTube link. Attempting to extract it from the message.")
+
+	url, err := extractUrl(message, ytRegex, true)
+
+	if err != nil {
+		l.Printf("There was an issue extracting the YouTube URL --> %s", err.Error())
+
+		msg := "There was an issue extracting the youtube link from your message. Please ensure your URL is valid"
+		err = sendTelegramMessage(bot, update, msg)
+
+		if err != nil {
+			l.Printf("Failed to send message to %s --> %s", username, err.Error())
+		} else {
+			l.Printf("Successfully send %s the following Telegram message --> %s", username, msg)
+		}
+	}
+
+	l.Printf("The extraction returned the following --> %+v", url)
+	//
+	////
+
+	// If the URL is valid, we need to extract the YT ID from it
+	//
+	l.Print("Attempting to extract the ID from the YouTube link.")
+
+	youtubeId, err := extractYouTubeId(url)
+
+	if err != nil {
+		l.Printf("There was an error trying to extract a YouTube ID from the given URL: %s", err.Error())
+		
+		msg := "There was an issue extracting the youtube ID from the link you sent. Please ensure your URL is valid."
+		err = sendTelegramMessage(bot, update, msg)
+
+		if err != nil {
+			l.Printf("Failed to send message to %s --> %s", username, err.Error())
+		} else {
+			l.Printf("Successfully sent %s the following Telegram message --> %s", username, msg)
+		}
+	}
+
+	l.Printf("The extracted YouTube ID was --> %s", youtubeId)
+	//
+	////
+
+	// Attempt to download the video. If it is already downloaded, we will send the downloaded file. 
+	//
+	l.Printf("Attempting to download YouTube video")
+
+	msg := "Attempting to download YouTube video. Please be patient as it can take a few minutes to finish the download."
+	err = sendTelegramMessage(bot, update, msg)
+
+	if err != nil {
+		l.Printf("Failed to send message to %s --> %s", username, err.Error())
+	} else {
+		l.Printf("Successfully sent %s the following Telegram message --> %s", username, msg)
+	}
+
+	downloadedVideo, err := downloadYouTubeVideo(message, youtubeId)
+
+	if err != nil {
+		l.Printf("There was an error trying to download the video: %s", err.Error())
+
+		msg := "Failed to download video. I appologize for the inconvenience. Please wait a little bit and try again later."
+		err = sendTelegramMessage(bot, update, msg)
+		 
+		if err != nil {
+			l.Printf("Failed to send message to %s --> %s", username, err.Error())
+		} else { 
+			l.Printf("Successfully sent %s the following Telegram message --> %s", username, msg)
+		}
+	}
+
+	l.Printf("The downloaded video was found --> %+v", downloadedVideo)
+	//
+	////
+
+	// Let's send a message letting the user know that the video is being sent
+	//
+	l.Printf("Attempting to send a message to %s to let them know we are attempting to send them a file", username)
+
+	msg = "Successfully downloaded the video. Attempting to send it to you. Please be patient as larger videos may take some time to send"
+	err = sendTelegramMessage(bot, update, msg)
+
+	// TODO: Add error handling here
+
+	l.Printf("Successfully sent the following message to %s --> %+v", username, msg)
+
+	//
+	////
+
+	// let's send the downloaded video to the user now
+	// func (b *Bot) SendVideo(ctx context.Context, params *SendVideoParams) (*Message, error)
+	//
+	l.Printf("Attempting to send video file to %s", username)
+	
+	err = SendTelegramVideo(bot, update, downloadedVideo)	
+
+	if err != nil {
+		l.Printf("An error occurred while sending the video --> %s", err)
+	}
+
+	l.Printf("Successfully sent the video to %s", username)
+	//
+	////
 }
