057409aacc1133f8338f20c4023ad0cc47f92662
Connor Hudson <connorqhudson2020@gmail.com>
2025-10-02 17:48:38 -0500

    create general_utils.go and add checkVideoAlreadyDownloaded func to it

---
+++ general_utils.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"path/filepath"
+)
+
+// if the video is already downloaded, then we can send the existing video
+func checkVideoAlreadyDownloaded(dir string, filename string) (string, error) {
+	var filePath string
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() == false {
+			// Get filename without extension since we don't necessarily know
+			// what the ext will be (mp4, webm, etc)
+			nameWithoutExt := strings.TrimSuffix(info.Name(), filepath.Ext(info.Name()))
+			if nameWithoutExt == filename {
+				filePath = path
+				return filepath.SkipDir
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return "", err
+	}
+	if filePath == "" {
+		return "", fmt.Errorf("Failed to find existing file with base name of %s", filename)
+	}
+	return filePath, nil
+}
